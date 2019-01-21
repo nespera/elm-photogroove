8f7266c552467cde1c539b0bd18c735bde3104e3
chrisr <c@24.org.uk>
2019-01-21 21:44:42 +0000

    Added selected url to the model

--- src/Photogroove.elm
+++ src/Photogroove.elm
@@ -8,17 +8,22 @@ urlPrefix = "http://elm-in-action.com/"
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ] (List.map viewThumbnail model)
+        , div [ id "thumbnails" ] (List.map (viewThumbnail model.selectedUrl) model.photos)
+        , img [ class "large", src (urlPrefix ++ "large/" ++ model.selectedUrl)][]
     ]
 
-viewThumbnail thumb =
-    img [ src (urlPrefix ++ thumb.url) ] []
+viewThumbnail selectedUrl thumb =
+    img [ src (urlPrefix ++ thumb.url)
+          , classList [ ( "selected", selectedUrl == thumb.url ) ]
+        ] []
 
-initialModel =
-    [ { url = "1.jpeg" }
-    , { url = "2.jpeg" }
-    , { url = "3.jpeg" }
-    ]
+initialModel = {
+        photos = [ { url = "1.jpeg" }
+            , { url = "2.jpeg" }
+            , { url = "3.jpeg" }
+        ]
+        , selectedUrl = "1.jpeg"
+    }
 
 main =
     view initialModel
